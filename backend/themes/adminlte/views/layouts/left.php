266615dcc2ea8bc266aaa364be0e70e77cbde0f2
Roman <romkafrfr@gmail.com>
2020-01-14 19:03:24 +0300

    Скрыл некоторые столбцы при выводе статей

--- backend/themes/adminlte/views/layouts/left.php
+++ backend/themes/adminlte/views/layouts/left.php
@@ -32,36 +32,36 @@ use common\models\Section;
 
         <?php
         $items = [
-            ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
-            ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
-            ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
-            ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
-            [
-                'label' => 'Some tools',
-                'icon' => 'share',
-                'url' => '#',
-                'items' => [
-                    ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii'],],
-                    ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug'],],
-                    [
-                        'label' => 'Level One',
-                        'icon' => 'circle-o',
-                        'url' => '#',
-                        'items' => [
-                            ['label' => 'Level Two', 'icon' => 'circle-o', 'url' => '#',],
-                            [
-                                'label' => 'Level Two',
-                                'icon' => 'circle-o',
-                                'url' => '#',
-                                'items' => [
-                                    ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                    ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-            ],
+//            ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
+//            ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
+//            ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
+//            ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
+//            [
+//                'label' => 'Some tools',
+//                'icon' => 'share',
+//                'url' => '#',
+//                'items' => [
+//                    ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii'],],
+//                    ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug'],],
+//                    [
+//                        'label' => 'Level One',
+//                        'icon' => 'circle-o',
+//                        'url' => '#',
+//                        'items' => [
+//                            ['label' => 'Level Two', 'icon' => 'circle-o', 'url' => '#',],
+//                            [
+//                                'label' => 'Level Two',
+//                                'icon' => 'circle-o',
+//                                'url' => '#',
+//                                'items' => [
+//                                    ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
+//                                    ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
+//                                ],
+//                            ],
+//                        ],
+//                    ],
+//                ],
+//            ],
         ];
 
         $items = array_merge($items, Section::getItemsSectionForLeftMenu());
