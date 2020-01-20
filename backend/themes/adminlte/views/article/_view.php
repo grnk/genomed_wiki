292f174e5baa472b240292840b8ce2de95dfd0c7
Roman <romkafrfr@gmail.com>
2020-01-20 12:35:25 +0300

    Добавил вкладку для предпросмотра статьи в выпадающем окне списка статей

---
+++ backend/themes/adminlte/views/article/_view.php
@@ -0,0 +1,10 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Article */
+?>
+
+<div style="background-color: #fff; padding: 10px">
+    <?= '<h2>' . $model->title . '</h2>' ?>
+    <?= $model->content ?>
+</div>
