c0ddd5a76f9d91dccbdf8703f6695324656a5281
Roman <romkafrfr@gmail.com>
2020-01-20 13:34:05 +0300

    перенёс стили в файл стилей бля backend задал white-space: normal; для всех дочерних элементов класса

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -13,6 +13,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/backend.css'
     ];
     public $js = [
         'js/basic.js',
