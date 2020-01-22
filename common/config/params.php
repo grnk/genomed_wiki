69bdf5236e24f89aa20eadb18d1535acd9e8ac58
Roman <romkafrfr@gmail.com>
2020-01-22 23:13:19 +0300

    установил расширение kartik-v/yii2-widget-fileinput добавил параметры

--- common/config/params.php
+++ common/config/params.php
@@ -1,5 +1,7 @@
 <?php
 return [
+    'bsDependencyEnabled' => false, // this will not load Bootstrap CSS and JS for all Krajee extensions
+    'bsVersion' => '3.x', // this will set globally `bsVersion` to Bootstrap 4.x for all Krajee Extensions
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'senderEmail' => 'noreply@example.com',
