bb4aacb4da3e8595938b4e64690b39e7b6198d04
Roman <romkafrfr@gmail.com>
2020-02-03 11:05:16 +0300

    начальная настройка ccs стилей для корректного отображения

--- backend/themes/adminlte/views/menu/index.php
+++ backend/themes/adminlte/views/menu/index.php
@@ -8,35 +8,5 @@ use backend\widgets\MenuRedactor; ?>
 
 <?= MenuRedactor::widget() ?>
 
-<!--<div class="sortable-section connectedSortable" data-item-id="0">-->
-<!---->
-<!--    <div class="item" data-id="201" data-order="1" data-parent-id="0">-->
-<!--        <div class="item-header">Раздел 1 ТЕСТ</div>-->
-<!--        <div class="item-body">-->
-<!--            <div class="sortable-section connectedSortable" data-item-id="201">-->
-<!---->
-<!--                <div class="item" data-id="202" data-order="1" data-parent-id="1">-->
-<!--                    <div class="item-header">Подраздел 1.1 ТЕСТ</div>-->
-<!--                    <div class="item-body">-->
-<!--                        <div class="sortable-section connectedSortable" data-item-id="202">-->
-<!---->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!---->
-<!--                <div class="item" data-id="203" data-order="2" data-parent-id="1">-->
-<!--                    <div class="item-header">Подраздел 1.2 ТЕСТ</div>-->
-<!--                    <div class="item-body">-->
-<!--                        <div class="sortable-section connectedSortable" data-item-id="203">-->
-<!---->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!---->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!---->
-<!--</div>-->
 
 
