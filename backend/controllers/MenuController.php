641b8a4a1c2dea78a262ccf82f2af06dc8bc6f94
Roman <romkafrfr@gmail.com>
2020-02-04 14:07:09 +0300

    добавил поведение для доступа к MenuController

--- backend/controllers/MenuController.php
+++ backend/controllers/MenuController.php
@@ -3,12 +3,31 @@
 namespace backend\controllers;
 
 use common\classes\SectionUpdater;
-use common\models\Section;
 use Yii;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 
 class MenuController extends Controller
 {
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'actions' => ['index', 'view', 'create', 'update', 'delete', 'add-section-article', 'articles'],
+                        'roles' => ['@']
+                    ],
+                    [
+                        'allow' => false
+                    ]
+                ]
+            ]
+        ];
+    }
+
     public function actionIndex()
     {
         return $this->render('index');
