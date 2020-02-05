1cf8a14ea78640b2f44cd60cbf782edd03bb75d1
Roman <romkafrfr@gmail.com>
2020-02-05 12:46:58 +0300

    Добавил блок для кнопок, исправил положение блока

--- backend/widgets/assetsMenuRedactor/js/menu-backend-sortable.js
+++ backend/widgets/assetsMenuRedactor/js/menu-backend-sortable.js
@@ -11,29 +11,24 @@ $( document ).ready(function() {
         .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
         .find( ".item-header" )
         .addClass( "ui-widget-header ui-corner-all" )
-        .prepend( "<span class='glyphicon glyphicon-minus item-toggle'></span>");
+        .prepend( "<span class='action-buttons'><span class='glyphicon glyphicon-minus item-toggle '></span></span>");
 
     $( ".item-toggle" ).on( "click", function() {
         var icon = $( this );
         icon.toggleClass("glyphicon-minus glyphicon-plus");
         icon.closest( ".item" ).find( ".item-body" ).toggle();
-    })
+    });
 
     // обработка события update
     $(".sortable-section").sortable({
         update: function(event, ui) {
-            //console.log(this);
-            //console.log(event);
             var section = $(this);
             var children = section.children();
-            console.log("data-item-id = " + section.attr("data-item-id"));
-            // console.log(section.children());
 
             var childrenIds = [];
             children.each(function () {
                 childrenIds[childrenIds.length] = $(this).attr("data-id");
             });
-            console.log("children array = " + childrenIds);
 
             var data_send = {"section-id" : section.attr("data-item-id"), 'item-ids' : childrenIds};
             $.ajax({
