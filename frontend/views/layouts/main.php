28ebdd63cd4063a89bb1918de8f048c295bb4c5c
Roman <romkafrfr@gmail.com>
2020-02-27 12:57:14 +0300

    убрал из шаблона надпись с хлебными крошками

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -54,7 +54,6 @@ AppAsset::register($this);
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    Breadcrumbs Alert
                     <?= Breadcrumbs::widget([
                         'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
                     ]) ?>
