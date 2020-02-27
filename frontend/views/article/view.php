51e632e729566a6b0d5b647f35985197afce9ad2
Roman <romkafrfr@gmail.com>
2020-02-27 12:34:29 +0300

    детальная страница для статьи

--- frontend/views/article/view.php
+++ frontend/views/article/view.php
@@ -1,12 +1,27 @@
 <?php
 
-use yii\widgets\DetailView;
+/* @var $this yii\web\View */
+/* @var $model Article */
 
-echo DetailView::widget([
-    'model' => $model,
-    'attributes' => [
-        'title',                                           // title свойство (обычный текст)
-        'content:html',                                // description свойство, как HTML
-        'created_at:datetime',                             // дата создания в формате datetime
-    ],
-]);
+//use yii\widgets\DetailView;
+use common\models\Article;
+
+$this->title = $model->title;
+
+
+//
+//echo DetailView::widget([
+//    'model' => $model,
+//    'attributes' => [
+//        'title',                                           // title свойство (обычный текст)
+//        'content:html',                                // description свойство, как HTML
+//        'created_at:datetime',                             // дата создания в формате datetime
+//    ],
+//]);
+?>
+
+<div class="article-detail-view">
+    <div class="article-detail-view-title"><h1><?= $model->title ?></h1></div>
+    <div class="article-detail-view-content"><?= $model->content ?></div>
+    <div class="article-detail-view-time"><?= Yii::$app->formatter->asDate($model->created_at, 'php:d.m.Y') ?></div>
+</div>
